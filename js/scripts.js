8004f27e68a8653657b7f72ff9529ab7e57e0db2
Kaili Nishihira and Robert Murray <robert@>
2017-07-19 10:41:26 -0700

    Generate a random number, rolls will add to turn score, turn scores will add to total points after clicking hold button, player 1 and player 2 created, first player to reach 100 total points wins.

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+//business logic
+
+function roll() {
+  return Math.floor(Math.random() * 6) + 1; //The maximum is inclusive and the minimum is inclusive
+}
+
+function Player(playerName, turnScore, totalScore, currentRoll) {
+  this.playerName = playerName;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+  this.currentRoll = currentRoll;
+}
+
+Player.prototype.turnTotal = function(){
+  if (this.currentRoll === 1) {
+    this.turnScore = 0;
+  } else {
+  this.turnScore += this.currentRoll;
+  }
+}
+
+Player.prototype.hold = function(){
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
+  if (this.totalScore >= 100) {
+    $("#win").text(this.playerName + " WINS!");
+  }
+}
+
+var player1 = new Player("Player 1", 0, 0, 0);
+var player2 = new Player("Player 2", 0, 0, 0);
+
+$(function() {
+  $("#player-one-roll").submit(function(event) {
+    event.preventDefault();
+
+    player1.currentRoll = roll();
+    player1.turnTotal();
+
+    $(".dice-one").text(player1.currentRoll);
+    $(".turn-total").text(player1.turnScore);
+  });
+
+  $("#player-one-hold").submit(function(event) {
+    event.preventDefault();
+
+    player1.hold();
+
+    $(".score-total").text(player1.totalScore);
+    $(".dice-one").text("");
+    $(".turn-total").text("");
+  });
+});
+
+$(function() {
+  $("#player-two-roll").submit(function(event) {
+    event.preventDefault();
+
+    player2.currentRoll = roll();
+    player2.turnTotal();
+
+    $(".dice-two").text(player2.currentRoll);
+    $(".turn-total-two").text(player2.turnScore);
+  });
+
+  $("#player-two-hold").submit(function(event) {
+    event.preventDefault();
+
+    player2.hold();
+
+    $(".score-total-two").text(player2.totalScore);
+    $(".dice-two").text("");
+    $(".turn-total-two").text("");
+  });
+
+});

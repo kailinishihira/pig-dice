bc889716d1ce2e72b6adcfc79f27b6a46dbe110e
Kaili Nishihira and Robert Murray <kaili@>
2017-07-19 15:39:40 -0700

    disable players button when it is no longer their turn.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 //business logic
-
 function roll() {
   return Math.floor(Math.random() * 6) + 1; //The maximum is inclusive and the minimum is inclusive
 }
 
-function Player(playerName, turnScore, totalScore, currentRoll) {
+function Player(playerName, turnScore, totalScore, currentRoll, myTurn) {
   this.playerName = playerName;
   this.turnScore = turnScore;
   this.totalScore = totalScore;
   this.currentRoll = currentRoll;
+  this.myTurn = myTurn;
 }
 
 Player.prototype.turnTotal = function(){
   if (this.currentRoll === 1) {
     this.turnScore = 0;
+    turnSwap();
   } else {
   this.turnScore += this.currentRoll;
   }
@@ -36,12 +37,27 @@ function reset(){
   $(".score-total").text(" ");
   $(".score-total-two").text(" ");
   $("#win").hide();
+  $(".pig").hide();
 }
 
+function turnSwap() {
+  if (player1Turn) {
+    $(".p1Button").prop('disabled', true);
+    $(".p2Button").prop('disabled', false);
+    player1Turn = false;
+  } else {
+    $(".p1Button").prop('disabled', false);
+    $(".p2Button").prop('disabled', true);
+    player1Turn = true;
+  }
+}
+
+var player1Turn = true;
 var player1 = new Player("Player 1", 0, 0, 0);
 var player2 = new Player("Player 2", 0, 0, 0);
 
 $(function() {
+  $(".p2Button").prop('disabled', true);
   $("#player-one-roll").submit(function(event) {
     event.preventDefault();
 
@@ -56,7 +72,7 @@ $(function() {
     event.preventDefault();
 
     player1.hold();
-
+    turnSwap();
     $(".score-total").text(player1.totalScore);
     $(".dice-one").text(" ");
     $(".turn-total").text(" ");
@@ -78,7 +94,7 @@ $(function() {
     event.preventDefault();
 
     player2.hold();
-
+    turnSwap();
     $(".score-total-two").text(player2.totalScore);
     $(".dice-two").text(" ");
     $(".turn-total-two").text(" ");
